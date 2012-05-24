434bafcae970c31589b8dca5280f825fbd091da1
Syafik <syafik@syafik.(none)>
2012-05-24 08:11:17 +0700

    initalizee

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MediaDeliveryPlatform::Application.config.secret_token = '5d54258aeda8ab106c0d65006494f8da5190947a19da9421308d3d9273c7a3c65023f7c3bdc741ce04c3d1668367a534ff7de27fd22c0612637155a86c03a430'
